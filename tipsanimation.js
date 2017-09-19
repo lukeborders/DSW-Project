fbce9435d7a7967a70af3c51c08902273bc3119c
programming-john <31251360+programming-john@users.noreply.github.com>
2017-09-19 09:39:11 -0700

    Create tipsanimation.js

---
+++ tipsanimation.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+	$("#solu1").click(function() {
+	 	$("#tipso1").toggle();
+	})
+	$("#solu2").click(function() {
+		$("#tipso2").toggle();
+	})
+	$("#solu3").click(function() {
+		$("#tipso3").toggle();
+	})
+	$("#solu4").click(function() {
+		$("#tipso4").toggle();
+	})
+	$("#solu5").click(function() {
+		$("#tipso5").toggle();
+	})
+	$("#solu6").click(function() {
+		$("#tipso6").toggle();
+	})	
+	$("#solu7").click(function() {
+		$("#tipso7").toggle();
+		
+	})
+});
