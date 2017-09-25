cd46fd266b3b0459fece4939a45b370cfaac23f1
Robert Weisz <robtweisz@gmail.com>
2017-09-25 00:09:16 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+                  $("#salid").click(function() {
+                                    $("#text").html("SALAD<br /> 1. Slice cabbage and Lettuce to salad consistency.<br />2. Cut tomatoes and other desired vegetables.<br /> 3. Toss the salad.<br />4. Add desired dressing.<br />5. Serve and enjoy!");
+                                 });
+                  $("#guac").click(function() {
+                                    $("#text").html("GUACAMOLE<br />1. Peel and remove seeds from 2-4 avocados.<br />2. With the spoon, mash up the avocados until you meet your desired consistency.<br />3. Add desired savory flavors such as tomatoes, spices, and onion.<br />4. Mix in the other ingredients.<br />5. Serve and enjoy!");
+                                    });
+                  $("#fry").click(function() {
+                                   $("#text").html("FRIES<br />1. Cut potatoes in desired fry shape.<br />2. Heat up deep fryer.<br />3. Dip the fries in the deep fryer, let it sit for 5-7 minutes.<br />4. Remove fries from oil, strain the fries.<br />5. Add salt.<br />6. Serve and enjoy!");
+                                   });
+                  $("#spaghett").click(function() {
+                                       $("#text").html("Spaghetti:<br />1.Boil water in a pot until bubbling<br />2.Add desired noodles to the pot<br />3.Make tomato sauce or use store bought sauce<br />4.Heat up the sauce in a separate pot<br />5.Wait until sauce is warm/hot and the noodles are soft<br />6.Add basil<br />7.Serve and enjoy!");
+                                  });
+                  $("#sand").click(function() {
+                                   $("#text").html("SANDWICH<br />1. Cut bread horizontally in half. Spread butter over bottom half.<br />2. Layer cheese, salami, lettuce, tomatoes, onion, ham and bell pepper on bread.<br />3. Drizzle with dressing. Top with remaining bread half. Secure loaf with picks. Cut into serving pieces.");
+                                       });
+                  $("#smooth").click(function() {
+                                     $("#text").html("Fruit Smoothie:<br />1.Add 1 1/2 cups of pineapple juice<br />2.Add a banana<br />3.Add any other desired fruits(blueberries and strawberries always recommended)<br />4.Add ice for liquid texture.<br />5.Blend until all mixed well together<br />6.Serve and enjoy!");
+                                   });
+                  $("#taco").click(function() {
+                                   $("#text").html("TACOS<br />1.Cook beef in 10-inch skillet over medium heat 8 to 10 minutes, stirring occasionally, until brown; drain.Stir salsa into beef.<br />2.Heat to boiling, stirring constantly; reduce heat to medium-low.<br />3.<Cook 5 minutes, stirring occasionally. Pour beef mixture into large serving bowl.<br />Heat taco shells as directed on package.<br />4.Serve taco shells with beef mixture, lettuce, tomato and cheese.<br />5. Serve and enjoy!");
+                                     });
+                  $("#waffle").click(function() {
+                                     $("#text").html("Toaster Waffles:<br />1.Unwrap frozen waffles<br />2.For best flavor, let them thaw out for about 3-5 minutes<br />3.Put in toaster<br />4.If not quite golden brown, put it back into the toaster until it is<br />5.Add syrup (and butter if you wish)<br />6.Serve and enjoy!");
+                                   });
+});
