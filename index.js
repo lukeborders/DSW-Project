77cd42abbfcce55acde971b6025db594907b40cf
Robert Weisz <robtweisz@gmail.com>
2017-09-25 09:20:33 -0700

    Update index.js

--- index.js
+++ index.js
@@ -9,18 +9,18 @@ $(document).ready(function(){
                                    $("#text").html("FRIES<br />1. Cut potatoes in desired fry shape.<br />2. Heat up deep fryer.<br />3. Dip the fries in the deep fryer, let it sit for 5-7 minutes.<br />4. Remove fries from oil, strain the fries.<br />5. Add salt.<br />6. Serve and enjoy!");
                                    });
                   $("#spaghett").click(function() {
-                                       $("#text").html("Spaghetti:<br />1.Boil water in a pot until bubbling<br />2.Add desired noodles to the pot<br />3.Make tomato sauce or use store bought sauce<br />4.Heat up the sauce in a separate pot<br />5.Wait until sauce is warm/hot and the noodles are soft<br />6.Add basil<br />7.Serve and enjoy!");
+                                       $("#text").html("SPAGHETTI<br />1.Boil water in a pot until bubbling<br />2.Add desired noodles to the pot<br />3.Make tomato sauce or use store bought sauce<br />4.Heat up the sauce in a separate pot<br />5.Wait until sauce is warm/hot and the noodles are soft<br />6.Add basil<br />7.Serve and enjoy!");
                                   });
                   $("#sand").click(function() {
                                    $("#text").html("SANDWICH<br />1. Cut bread horizontally in half. Spread butter over bottom half.<br />2. Layer cheese, salami, lettuce, tomatoes, onion, ham and bell pepper on bread.<br />3. Drizzle with dressing. Top with remaining bread half. Secure loaf with picks. Cut into serving pieces.");
                                        });
                   $("#smooth").click(function() {
-                                     $("#text").html("Fruit Smoothie:<br />1.Add 1 1/2 cups of pineapple juice<br />2.Add a banana<br />3.Add any other desired fruits(blueberries and strawberries always recommended)<br />4.Add ice for liquid texture.<br />5.Blend until all mixed well together<br />6.Serve and enjoy!");
+                                     $("#text").html("FRUIT SMOOTHIE<br />1.Add 1 1/2 cups of pineapple juice<br />2.Add a banana<br />3.Add any other desired fruits(blueberries and strawberries always recommended)<br />4.Add ice for liquid texture.<br />5.Blend until all mixed well together<br />6.Serve and enjoy!");
                                    });
                   $("#taco").click(function() {
                                    $("#text").html("TACOS<br />1.Cook beef in 10-inch skillet over medium heat 8 to 10 minutes, stirring occasionally, until brown; drain.Stir salsa into beef.<br />2.Heat to boiling, stirring constantly; reduce heat to medium-low.<br />3.<Cook 5 minutes, stirring occasionally. Pour beef mixture into large serving bowl.<br />Heat taco shells as directed on package.<br />4.Serve taco shells with beef mixture, lettuce, tomato and cheese.<br />5. Serve and enjoy!");
                                      });
                   $("#waffle").click(function() {
-                                     $("#text").html("Toaster Waffles:<br />1.Unwrap frozen waffles<br />2.For best flavor, let them thaw out for about 3-5 minutes<br />3.Put in toaster<br />4.If not quite golden brown, put it back into the toaster until it is<br />5.Add syrup (and butter if you wish)<br />6.Serve and enjoy!");
+                                     $("#text").html("TOASTER WAFFLES<br />1.Unwrap frozen waffles<br />2.For best flavor, let them thaw out for about 3-5 minutes<br />3.Put in toaster<br />4.If not quite golden brown, put it back into the toaster until it is<br />5.Add syrup (and butter if you wish)<br />6.Serve and enjoy!");
                                    });
 });
