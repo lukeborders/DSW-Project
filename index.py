f324d6b5edbe3924796111c6b6fe93b4f2a8ffc3
Luke Borders <31492762+lukeborders@users.noreply.github.com>
2017-10-03 09:40:30 -0700

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,14 @@
+from flask import Flask, url_for, render_template
+app = Flask(__name__)
 
+@app.route("/")
+def render_home():
+  return render_template('index.html')
+@app.route("/p1")
+def render_page1():
+  return render_template('page1.html')
+@app.route("/p2")
+def render_page2():
+  return render_template('page2/html')
+if __name__ == "__main__":
+  app.run(debug=False, port=54321)
